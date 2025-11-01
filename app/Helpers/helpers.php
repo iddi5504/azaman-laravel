00ed294b524956ab1fe52e2d64dd75f6dce2ac36
iddi <iddi5504@gmail.com>
2025-11-01 05:20:34 +0000

    document code

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -2,6 +2,11 @@
 
 /**
  * This function creates a php array for our flash messages
+ * @param string $status The status of the message . Eg. success, error, warning
+ * @param string $message The message 
+ * @param string $description Additional description to add
+ * @return array The flash message object to be stored in session
+ * 
  */
 function getFlashMessageObject(string $status, string $message, string $description = '')
 {
