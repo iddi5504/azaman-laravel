e288052fd8a34fff01a480f71b88263c03645000
iddi <iddi5504@gmail.com>
2025-11-01 05:10:20 +0000

    document code

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * This function creates a php array for our flash messages
+ */
 function getFlashMessageObject(string $status, string $message, string $description = '')
 {
     return [
